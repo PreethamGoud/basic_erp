9e85fb1a5b99db09b0a1c99a9df987d62c06aafc
Fahim Ali Zain TP <faztp12@gmail.com>
2018-04-08 11:50:18 +0530

    Replaced essential with vie-bootstrap

--- vue-basic_erp-src/src/main.js
+++ vue-basic_erp-src/src/main.js
@@ -3,10 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(ElementUI)
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
